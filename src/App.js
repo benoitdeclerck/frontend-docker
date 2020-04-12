d2ae08cf264e5821b3165832b0a34e1231ff60ef
benoitdeclerck <mail@benoitdeclerck.tv>
2020-04-13 00:00:00 +0200

    Testing AWS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Yes it does.
-         Yes it does.
+         This doesn't seem to be working...
         </p>
         <a
           className="App-link"
